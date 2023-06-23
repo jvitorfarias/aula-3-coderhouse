efdf313e1f32c21770ac5030b9e7ed586506c417
jvitorfarias <joaovitorfarias4@gmail.com>
2023-06-23 15:58:22 -0300

    first commit of this example

---
+++ aula3.js
@@ -0,0 +1,10 @@
+let maiorIdade = 1;
+let parabens = "Parabéns, você atingiu a maior idade e já pode tirar sua CNH!"
+
+while (maiorIdade < 19) {
+  console.log("você tem: " + maiorIdade + " anos de idade");
+  
+  maiorIdade ++
+}
+
+console.log(parabens)
